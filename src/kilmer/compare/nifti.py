51dbc3a3918932e5b3d3230004232f28d15ed457
Timothy O'Keefe <timothy_okeefe@harvard.edu>
2026-04-23 17:43:38 -0400

    failover to using zdiff on gzipped NIfTI files

--- src/kilmer/compare/nifti.py
+++ src/kilmer/compare/nifti.py
@@ -1,9 +1,10 @@
 import logging
 import filecmp
+import subprocess as sp
 
 logger = logging.getLogger(__name__)
 
-def cmp(a, b):
+def cmp(a, b, try_zdiff=True):
     ''' 
     Compare two NIfTI files {a} and {b} and return True if they are the same
     or False if they are different.
@@ -11,6 +12,9 @@ def cmp(a, b):
     If both input files are symlinks, return True if both links point to the 
     same file. If both input files are real files, return True if both files 
     exist and contents are identical.
+
+    If filecmp says the files are different, pass try_zdiff=True to test files
+    equality using zdiff.
     '''
     if a.is_symlink() and b.is_symlink():
         a = a.readlink()
@@ -24,4 +28,22 @@ def cmp(a, b):
     if not b.exists():
         logger.warning(f'file not found {b}')
         return False
-    return filecmp.cmp(a, b, shallow=False)
+    result = filecmp.cmp(a, b, shallow=False)
+    if not result and try_zdiff:
+        return zdiff(a, b)
+    return result
+
+def zdiff(a, b):
+    ''' run zdiff '''
+    cmd = [
+        'zdiff',
+        a,
+        b
+    ]
+    cmds = sp.list2cmdline(cmd)
+    logger.debug(f'running {cmds}')
+    returncode = sp.call(cmd)
+    if returncode == 0:
+        return True
+    return False
+
